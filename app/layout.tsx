c68a7b25a4c4b27c36412749967f8547278e67b3
Tuan Do <tuan.dominh@ivisgroup.com>
2024-06-17 09:50:54 +0700

    chapter 16: metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
+import { Metadata } from 'next';
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Tuan Do Dashboard',
+    default: 'Tuan Do Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
 export default function RootLayout({
   children,
 }: {
